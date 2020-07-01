65e6013cc51f3253f86d3deb32b0d26ff705f5d6
Arthur Cipolari <arthur.cipolari@gmail.com>
2020-07-01 13:41:21 -0300

    Update create-table.js

--- HospitalAPI/create-table.js
+++ HospitalAPI/create-table.js
@@ -213,20 +213,6 @@ function triggerHospitalization(connection){
 }
 
 
-// function addRows(conn){
-//     const sql = "INSERT INTO Clientes(Nome,CPF) VALUES ?";
-//     const values = [
-//           ['teste1', '12345678901'],
-//           ['teste1', '09876543210'],
-//           ['teste3', '12312312399']
-//         ];
-//     conn.query(sql, [values], function (error, results, fields){
-//             if(error) return console.log(error);
-//             console.log('adicionou registros!');
-//             conn.end();//fecha a conexão
-//         });
-//   }
-
 connection.connect(function(err){
     if(err) return console.log(err);
     console.log('conectou!');
